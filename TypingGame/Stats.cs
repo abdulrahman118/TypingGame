7504fa0f10e99659e05cd3e042280b0ac2f3fb03
Abdul Rahman <developer.abdulrahman@gmail.com>
2021-01-21 06:31:27 +0530

    initial commit

---
+++ TypingGame/Stats.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingGame
+{
+    class Stats
+    {
+        public int Total { get; set; }
+
+        public int Missed { get; set; }
+
+        public int Correct { get; set; }
+
+        public int Accuracy { get; set; }
+
+        public void Update(bool isCorrectKey)
+        {
+            Total++;
+
+            if (!isCorrectKey)
+            {
+                Missed++;
+            }
+            else
+            {
+                Correct++;
+            }
+
+            Accuracy = 100 * Correct / (Missed + Correct);
+        }
+    }
+}
